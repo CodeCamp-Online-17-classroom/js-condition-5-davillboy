865f70110c9e7e2002eb2cc34d3e19916db2496d
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 01:26:22 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // write code here
+   <script>
+        let input = prompt("Enter a number");
+
+        if (!isNaN(input)) {
+        input = Number(input); 
+        if (input > 0) {
+            alert("Positive number");
+        } else if (input === 0) {
+            alert("Zero");
+        } else {
+            alert("Negative number");
+        }
+        } else {
+        alert("Invalid number");
+        }
+
+    </script>
